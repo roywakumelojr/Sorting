feaae45bb67aa53f6747d7d593618baf917e9a28
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-18 18:03:52 -0500

    Helper function and merge sort complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,13 +3,41 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     merged_arr = [0] * elements
     # TO-DO
+    arrA_index = 0
+    arrB_index = 0
+    merged_index = 0
+
+    while arrA_index < len(arrA) and arrB_index < len(arrB):
+        if arrA[arrA_index] <= arrB[arrB_index]:
+            merged_arr[merged_index] = arrA[arrA_index]
+            arrA_index += 1
+        else:
+            merged_arr[merged_index] = arrB[arrB_index]
+            arrB_index += 1
+
+        merged_index +=1
     
+    while arrB_index < len(arrB):
+        merged_arr[merged_index] = arrB[arrB_index]
+        arrB_index += 1
+        merged_index += 1
+
+    while arrA_index < len(arrA):
+        merged_arr[merged_index] = arrA[arrA_index]
+        arrA_index += 1
+        merged_index += 1   
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if len(arr) > 1:
+        half_index = len(arr) // 2
+        left_arr_half = arr[:half_index]
+        right_arr_half = arr[half_index:]
+        return merge(merge_sort(left_arr_half), merge_sort(right_arr_half))
 
     return arr
 
